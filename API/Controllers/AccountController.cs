b27b0560ac0abc64550a0719ecb6e2d188237bb0
Jaleela Abubakari <2016083616@ufs4life.ac.za>
2021-04-29 14:22:24 +0200

    End of Section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -36,10 +36,9 @@ namespace API.Controllers
 
             using var hmac = new HMACSHA512();
 
-
-                user.UserName = registerDto.Username.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-                user.PasswordSalt = hmac.Key;
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -48,7 +47,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -82,7 +82,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
             
         }
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using API.DTOs;
 using AutoMapper;
 using API.Helpers;
+using System;
 
 namespace API.Data
 {
@@ -29,12 +30,16 @@ namespace API.Data
 
         public async Task<AppUser> GetUserByUsernameAsync(string username)
         {
-            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+            return await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users
+            .Include(p => p.Photos)
+            .ToListAsync();
         }
 
         public async Task<bool> SaveAllAsync()
@@ -47,11 +52,27 @@ namespace API.Data
             _context.Entry(user).State = EntityState.Modified;
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-              return await _context.Users
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .ToListAsync();  
+              var query = _context.Users.AsQueryable();
+
+                query = query.Where(u => u.UserName != userParams.CurrentUsername);
+                query = query.Where(u => u.Gender != userParams.Gender);
+                
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+                
+                query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+                
+                query = userParams.OrderBy switch 
+                {   
+                    "created" => query.OrderByDescending(u => u.Created),
+                    _ => query.OrderByDescending(u => u.LastActive)
+                };
+
+                return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper
+                .ConfigurationProvider).AsNoTracking(), 
+                userParams.PageNumber, userParams.PageSize);    
         }
         public async Task<MemberDto> GetMemberAsync(string username)
         {
