423da2c2fadfdaa6c9362973c4619852fd3111fb
Jaleela Abubakari <2016083616@ufs4life.ac.za>
2021-05-13 09:06:05 +0200

    End of Section 16

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using API.Data;
 
 
 namespace API.Extensions
@@ -12,6 +16,17 @@ namespace API.Extensions
         public static IServiceCollection AddIdentityServices(this IServiceCollection services,
                                                             IConfiguration config)
         {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+             opt.Password.RequireNonAlphanumeric = false;
+        })
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddSignInManager<SignInManager<AppUser>>()
+            .AddRoleValidator<RoleValidator<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -22,6 +37,11 @@ namespace API.Extensions
                         ValidateIssuer = false,
                         ValidateAudience = false
                     };
+                });
+
+                services.AddAuthorization(opt => {
+                    opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                    opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
                 });
 
             return services;
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -36,6 +36,9 @@ currentUser$ = this.currentUserSource.asObservable();
     )
   }
   setCurrentUser(user: User){
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -43,4 +46,8 @@ currentUser$ = this.currentUserSource.asObservable();
     localStorage.removeItem('user');
     this.currentUserSource.next(null);//nullinbrackets
   }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
+  }  
 }
